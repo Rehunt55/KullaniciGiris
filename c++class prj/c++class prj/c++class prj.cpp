ffa521218ba6fc5de54c16433c144ec0b1fd7536
Rehunt55 <alprnyapc55@gmail.com>
2022-12-13 15:42:56 +0300

    KullaniciGiris

---
+++ c++class prj/c++class prj/c++class prj.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Login {
+private:
+    string username;
+    string password;
+public:
+    Login(string username, string password) {
+        this->username = username;
+        this->password = password;
+    }
+
+    bool checkLogin() {
+        if (username == "admin" && password == "1234") {
+            return true;
+        }
+        return false;
+    }
+};
+
+int main() {
+    string username, password;
+
+    cout << "Kullanici adinizi giriniz: ";
+    cin >> username;
+
+    cout << "Sifrenizi giriniz: ";
+    cin >> password;
+
+    Login login(username, password);
+
+    if (login.checkLogin()) {
+        cout << "Giris basarili!" << endl;
+    }
+    else {
+        cout << "Kullanici adi veya sifre hatali!" << endl;
+    }
+
+    return 0;
+}
+
+
+
